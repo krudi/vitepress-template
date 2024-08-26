4725848079ae2f2b749754f4d673b4fbb40c9b83
Patryk Kudlik <patryk.kudlik@gmail.com>
2024-08-26 06:27:06 +0200

    fix: reorganize Prettier configuration

---
+++ prettier.config.mjs
@@ -0,0 +1,20 @@
+/**
+ * @type {import('prettier').Config}
+ */
+export const config = {
+    printWidth: 80,
+    tabWidth: 2,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    arrowParens: 'always',
+    bracketSameLine: false,
+    singleAttributePerLine: false,
+    rangeStart: 0,
+    rangeEnd: 1,
+    requirePragma: true,
+    insertPragma: true,
+    proseWrap: 'always',
+};
